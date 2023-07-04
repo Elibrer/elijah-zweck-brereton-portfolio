8b91f6616c4afdc8ee3c410f544d13e4b3c5ed65
Elibrer <eli.brer@gmail.com>
2023-07-04 22:05:17 +0930

    Finished optimising for different screen sizes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,12 +11,9 @@ const About = () => {
           <h4>Full-stack | HTML, CSS, JavaScript</h4>
           <div>
             <p className="aboutContent">
-              I'm Eli, a full-stack developer based in Adelaide specialising in
-              the MERN software stack, which includes MongoDb, Express.js,
-              React, and Node.js. I'm passionate about learning, and I believe
-              that increasing my knowledge and skills provides me with the tools
-              to give my clients the best possible service. Fill out the form in
-              the contact page if you'd like to get in contact with me.
+            I'm Eli, a full-stack developer specialising in the MERN software stack, which includes MongoDB, Express.js, React, and Node.js. With expertise in these technologies, I build robust and scalable web applications. I'm passionate about continuous learning and constantly strive to enhance my skills to provide the best possible service to my clients.
+            <br/>
+            If you have any questions, project inquiries, or would like to get in touch, feel free to reach out to me through the contact form on my website. I believe that effective communication is key to successful collaborations, and I look forward to discussing how I can contribute to your project's success.
             </p>
           </div>
         </div>
