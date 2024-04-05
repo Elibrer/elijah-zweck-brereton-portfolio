0c9441d40b262d50be07e81364ce17e955a1cfb6
Elibrer <eli.brer@gmail.com>
2024-04-05 21:22:44 +0200

    Fixed issue with build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,4 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/elijah-zweck-brereton-portfolio', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-
-// module.exports = mongoose.connection;
-
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/elijah-zweck-brereton-portfolio', {
   useNewUrlParser: true,
@@ -18,16 +7,27 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/elijah-zw
   useFindAndModify: false,
 });
 
-const db = mongoose.connection;
+module.exports = mongoose.connection;
 
-// Event listener for successful connection
-db.on("connected", () => {
-  console.log("Connected to MongoDB Atlas");
-});
+// const mongoose = require("mongoose");
 
-// Event listener for connection error
-db.on("error", (err) => {
-  console.error("Error connecting to MongoDB Atlas:", err);
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/elijah-zweck-brereton-portfolio', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
-module.exports = db;
+// const db = mongoose.connection;
+
+// // Event listener for successful connection
+// db.on("connected", () => {
+//   console.log("Connected to MongoDB Atlas");
+// });
+
+// // Event listener for connection error
+// db.on("error", (err) => {
+//   console.error("Error connecting to MongoDB Atlas:", err);
+// });
+
+// module.exports = db;
